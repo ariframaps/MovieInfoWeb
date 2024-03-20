8e5bf69fc0e9910b4ef1d8f6c16c5f92617fdb69
ariframaps <ariframa@student.ub.ac.id>
2024-03-20 08:54:35 +0700

    membuat elemen card untuk movie bekerja untuk setiap data yang diterima akan ditampilkan

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 $.ajax({
-    url: 'http://www.omdbapi.com/?apikey=5e5bd661&s=avengers',
+    // url: 'http://www.omdbapi.com/?apikey=5e5bd661&s=avengers',
     success: results => {
-        console.log(results);
+        let movies = results.Search
+
+        let cards = ''
+
+        movies.forEach(mov => {
+            cards += `  <div class="col-md-4 my-5">
+                            <div class="card">
+                                <img src="" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">2024</p>
+                                    <a href="#" class="btn btn-primary">Show more</a>
+                                </div>
+                            </div>
+                        </div>`;
+        });
+        $('#card-container').html(cards);
+
     },
     error: (e) => {
         console.log(e.responseText)
